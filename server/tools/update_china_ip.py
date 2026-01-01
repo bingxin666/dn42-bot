7e35a7160f126e99d1781b3b433a2bd4d13e78ee
Bingxin <73421892+bingxin666@users.noreply.github.com>
2026-01-01 10:09:18 +0000

    更新中国IP地址源链接

--- server/tools/update_china_ip.py
+++ server/tools/update_china_ip.py
@@ -6,14 +6,14 @@ import requests
 
 def update_china_ip():
     try:
-        ChinaIPv4_RAW = requests.get("https://ruleset.skk.moe/Clash/ip/china_ip.txt", timeout=30).text
+        ChinaIPv4_RAW = requests.get("https://raw.githubusercontent.com/bingxin666/china-ip-list/master/chnroute.txt", timeout=30).text
         base.ChinaIPv4 = [
             IPv4Network(i.strip()) for i in ChinaIPv4_RAW.splitlines() if i != "" and not i.startswith("#")
         ]
     except BaseException:
         pass
     try:
-        ChinaIPv6_RAW = requests.get("https://ruleset.skk.moe/Clash/ip/china_ip_ipv6.txt", timeout=30).text
+        ChinaIPv6_RAW = requests.get("https://raw.githubusercontent.com/bingxin666/china-ip-list/master/chnroute_v6.txt", timeout=30).text
         base.ChinaIPv6 = [
             IPv6Network(i.strip()) for i in ChinaIPv6_RAW.splitlines() if i != "" and not i.startswith("#")
         ]
